f87cbef53e37a3dbab2f58629540d1831368b165
Nilesh Kumar Singh <singhnilesh516@gmail.com>
2023-01-01 13:51:49 +0530

    Final push

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,7 +2,6 @@ import React , {useState } from "react";
 
 export default function TextForm(props) {
     const handleOnchange = (event)=> {
-        // console.log("handleOnchange");
         setText(event.target.value);
     }
 
@@ -30,41 +29,35 @@ const handleExtraSpace = ()=> {
   props.showAlert("Extera spaces removed!","success")
 }
 const handleCopy = ()=> {
-    let newText = document.getElementById("textBox");
-    newText.select();
-    navigator.clipboard.writeText(newText.value);
+    // let newText = document.getElementById("textBox");
+    // newText.select();
+    navigator.clipboard.writeText(text);
+    // document.getSelection().removeAllRanges();
     props.showAlert("Copied to clipboard!","success")
   
 }
-
-// const handleReverseClick = ()=> {
-//   
-//   let newText = text;
-//   setText(newText);
-// }
 
     const [text, setText] = useState("");
 
   return (
     <>
     <div className={`text-${props.mode==="light"?"#6c757d":"light"} bg-${props.mode==="light"?"light":"#6c757d"}`}>
-    <h1>{props.heading}</h1>
+    <h1 className="mb-2">{props.heading}</h1>
       <div className="mb-3">
         <textarea className="form-control" value = {text} onChange={handleOnchange} id="textBox" rows="6" placeholder="Enter text here"></textarea>
       </div>
-      <button type="button" className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
-      <button type="button" className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to lowercase</button>
-      {/* <button type="button" className="btn btn-primary mx-1" onClick={handleReClick}>Reverse Text</button> */}
-      <button type="button" className="btn btn-primary mx-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
-      <button type="button" className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-      <button type="button" className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
+      <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to UPPERCASE</button>
+      <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to lowercase</button>
+      <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+      <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+      <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className={`container text-${props.mode==="light"?"#6c757d":"light"} bg-${props.mode==="light"?"light":"#6c757d"}my-3`}>
       <h2>Your input Summary</h2>
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-      <p> {0.008 * text.split(" ").length} Minutes read </p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+      <p> {0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read </p>
       <h2>Preview</h2>
-      <p>{text.length>0 ? text : "Enter your text in the above box to preview here"}</p>
+      <p>{text.length>0 ? text : "Nothing to preview!"}</p>
     </div>
     </>
   );
